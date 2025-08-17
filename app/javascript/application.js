9fdfe849218098bc8f323b5c04bed40d21eaa1c3
zachjustice <me@zachjustice.dev>
2025-08-17 18:11:45 -0400

    Use js controllers and hotwire or whatever

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,6 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import $ from 'jquery';
-import { createConsumer } from "@rails/actioncable"
 // import "channels"
 // import consumer from "./consumer"
 
@@ -11,39 +10,38 @@ window.$ = $;
 window.jQuery = $;
 
 console.log('application.js')
-console.log('waiting room');
 
-createConsumer().subscriptions.create("WaitingRoomChannel", {
-  connected: function() {
-    // FIXME: While we wait for cable subscriptions to always be finalized before sending messages
-    return setTimeout(() => {
-      this.followCurrentMessage();
-      return this.installPageChangeCallback();
-    }, 1000);
-  },
-  received: function(data) {
-    console.log('data', data)
-    return $("[data-channel='waiting-room']").append(`<li>${data.newUser}`)
-  },
-  userIsCurrentUser: function(comment) {
-    return $(comment).attr('data-user-id') === $('meta[name=current-user]').attr('id');
-  },
-  followCurrentMessage: function() {
-    const roomId = $("[data-channel='waiting-room']").data('room-id')
-    if (roomId) {
-      return this.perform('follow', {
-        room_id: roomId
-      });
-    } else {
-      return this.perform('unfollow');
-    }
-  },
-  installPageChangeCallback: function() {
-    if (!this.installedPageChangeCallback) {
-      this.installedPageChangeCallback = true;
-      return $(document).on('turbolinks:load', function() {
-        return App.comments.followCurrentMessage();
-      });
-    }
-  }
-});
+// createConsumer().subscriptions.create("GameStateChannel", {
+//   connected: function() {
+//     // FIXME: While we wait for cable subscriptions to always be finalized before sending messages
+//     return setTimeout(() => {
+//       this.followCurrentMessage();
+//       return this.installPageChangeCallback();
+//     }, 1000);
+//   },
+//   received: function(data) {
+//     console.log('data', data)
+//     return $("[data-channel='waiting-room']").append(`<li>${data.newUser}`)
+//   },
+//   userIsCurrentUser: function(comment) {
+//     return $(comment).attr('data-user-id') === $('meta[name=current-user]').attr('id');
+//   },
+//   followCurrentMessage: function() {
+//     const roomId = $("[data-channel='waiting-room']").data('room-id')
+//     if (roomId) {
+//       return this.perform('follow', {
+//         room_id: roomId
+//       });
+//     } else {
+//       return this.perform('unfollow');
+//     }
+//   },
+//   installPageChangeCallback: function() {
+//     if (!this.installedPageChangeCallback) {
+//       this.installedPageChangeCallback = true;
+//       return $(document).on('turbolinks:load', function() {
+//         return App.comments.followCurrentMessage();
+//       });
+//     }
+//   }
+// })
--- app/jobs/JoinRoomJob.rb
+++ app/jobs/JoinRoomJob.rb
@@ -2,7 +2,7 @@ class JoinRoomJob < ApplicationJob
   def perform(user)
     puts("rooms:#{user.room_id}:new-user")
     ActionCable.server.broadcast(
-      "rooms:#{user.room_id}:new-user",
+      "rooms:#{user.room_id.to_i}:new-user",
       { newUser: user.name }
       # TODO: return html?
       # room: RoomsController.render(partial: "rooms/room", locals: { user: user })
