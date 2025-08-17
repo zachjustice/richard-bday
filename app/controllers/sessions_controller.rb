8e99404441698369dd50fa506908edab8fb71da6
zachjustice <me@zachjustice.dev>
2025-08-17 18:18:47 -0400

    Check for session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,21 @@ class SessionsController < ApplicationController
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
 
   def new
+    # check for current session before allowing user to start a new session.
+    # i.e. if user hits back or revisits this page.
+    if session[:user_id]
+      redirect_to after_authentication_url
+    end
   end
 
   def create
+    # check for current session before allowing user to start a new session.
+    # i.e. if user hits back or revisits this page.
+    if session[:user_id]
+      return redirect_to after_authentication_url
+    end
+
+    # Get room
     room = Room.find_by(params.permit(:code))
     if room.nil?
       return redirect_to new_session_path, alert: "Wrong room code."
@@ -18,8 +30,6 @@ class SessionsController < ApplicationController
     user = User.new(name: params[:name], room_id: room.id)
     if user.save
       session[:user_id] = user.id
-      puts("Setting user #{session[:user_id]}")
-      # publish_user_joined_room(room.id, user.id)
       start_new_session_for user
       redirect_to after_authentication_url
     else
@@ -30,12 +40,5 @@ class SessionsController < ApplicationController
   def destroy
     terminate_session
     redirect_to new_session_path
-  end
-
-  private
-
-  def publish_user_joined_room(room_id, user_id)
-    event = UserJoinedRoom.new(data: { user_id: user_id, room_id: room_id })
-    event_store.publish(event, stream_name: room_id)
   end
 end
---
+++ app/javascript/controllers/room_controller.js
@@ -0,0 +1,47 @@
+
+import { createConsumer } from "@rails/actioncable"
+import { Controller } from "@hotwired/stimulus"
+console.log('Room Controller')
+
+export default class extends Controller {
+  connect() {
+    console.log('Room Controller. connect')
+    createConsumer().subscriptions.create("WaitingRoomChannel", {
+      connected: function () {
+        // FIXME: While we wait for cable subscriptions to always be finalized before sending messages
+        return setTimeout(() => {
+          console.log('connecting...')
+          this.followCurrentMessage();
+          return this.installPageChangeCallback();
+        }, 1000);
+      },
+      received: function (data) {
+        console.log('received', data)
+        return $("[data-channel='waiting-room']").append(`<li>${data.newUser}`)
+      },
+      userIsCurrentUser: function (comment) {
+        return $(comment).attr('data-user-id') === $('meta[name=current-user]').attr('id');
+      },
+      followCurrentMessage: function () {
+        const roomId = $("[data-channel='waiting-room']").data('room-id')
+        console.log('follow', roomId)
+        if (roomId) {
+          return this.perform('follow', {
+            room_id: roomId
+          });
+        } else {
+          return this.perform('unfollow');
+        }
+      },
+      installPageChangeCallback: function () {
+        console.log('installPageChangeCallback')
+        if (!this.installedPageChangeCallback) {
+          this.installedPageChangeCallback = true;
+          return $(document).on('turbolinks:load', function () {
+            return App.comments.followCurrentMessage();
+          });
+        }
+      }
+    });
+  }
+}
