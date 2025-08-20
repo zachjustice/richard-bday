297c835c6b39c3a44a9020dc88445836687ed785
zachjustice <me@zachjustice.dev>
2025-08-19 21:27:43 -0400

    Winner on the results page

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -14,7 +14,35 @@ class PromptsController < ApplicationController
   end
 
   def results
+    prompt_id = params[:id]
     @prompt = Prompt.find_by(params.permit(:id))
     @votes = Vote.where(room_id: @current_room.id, prompt_id: params[:id])
+    answers = Answer.where(room_id: @current_room.id, prompt_id: params[:id])
+    @answers_by_id = answers.reduce({}) { |result, curr|
+      result[curr.id] = curr
+      result
+    }
+
+    num_users_in_room = User.where(room_id: @current_room.id).count
+    submitted_votes = Vote.where(prompt_id: prompt_id, room_id: @current_room.id)
+    @votes_by_answer = {}
+    most_votes = -1
+    @winners = []
+
+    # Only calculate winners if all the votes are in. Front-end will use existence of @winners to determine if voting is done.
+    if num_users_in_room <= submitted_votes.size
+      submitted_votes.each do |vote|
+        if @votes_by_answer[vote.answer_id].nil?
+          @votes_by_answer[vote.answer_id] = []
+        end
+        @votes_by_answer[vote.answer_id].push(vote)
+        if @votes_by_answer[vote.answer_id].size > most_votes
+          most_votes = @votes_by_answer[vote.answer_id].size
+          @winners = [ @answers_by_id[vote.answer_id] ]
+        elsif @votes_by_answer[vote.answer_id].size == most_votes
+          @winners.push(@answers_by_id[vote.answer_id])
+        end
+      end
+    end
   end
 end
