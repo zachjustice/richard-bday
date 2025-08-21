c25046bd74863f62cd28cd137e0b0102bf67764f
zachjustice <me@zachjustice.dev>
2025-08-20 21:48:14 -0400

    Redirect to next stage

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -15,11 +15,31 @@ class PromptsController < ApplicationController
   end
 
   def waiting
+    users_in_room = User.where(room_id: @current_room.id).count
+    submitted_answers = Answer.where(prompt_id: params[:id], room_id: @current_room.id).count
+    redirect_to_voting = submitted_answers >= users_in_room
+
+    # All answers have been collected, time to vote
+    if redirect_to_voting
+      return redirect_to controller: "prompts", action: "voting", id: params[:id]
+    end
+
     @prompt = Prompt.find_by(params.permit(:id))
     @users_with_submitted_answers = Answer.where(room_id: @current_room.id, prompt_id: params[:id]).map { |r| r.user.name }
   end
 
   def voting
+    exists = Vote.exists?(
+      prompt_id: params[:id],
+      user_id: @current_user.id,
+      room_id: @current_room.id
+    )
+
+    # All answers have been collected, time to vote
+    if exists
+      return redirect_to controller: "prompts", action: "results", id: params[:id]
+    end
+
     @prompt = Prompt.find_by(params.permit(:id))
     @answers = Answer.where(room_id: @current_room.id, prompt_id: params[:id])
   end
