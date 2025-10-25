95c49ff0f70dab23de479ed3f572f86786ecbcf6
zachjustice <me@zachjustice.dev>
2025-10-25 18:45:18 -0400

    remove commented out code

--- app/javascript/controllers/rooms_status_controller.js
+++ app/javascript/controllers/rooms_status_controller.js
@@ -6,14 +6,10 @@ export default class extends Controller {
   connect() {
     RoomMessageHub.register(EventType.NewUser, (event) => {
       console.log("RoomMessageHub:RoomController:Listener:NewUser", event)
-      // DOM update handled automatically by Turbo Streams
     })
 
     RoomMessageHub.register(EventType.AnswerSubmitted, (event) => {
       console.log("RoomMessageHub:PromptController:Listener:AnswerSubmitted", event)
-      if ($(`#submitted-answer-${event.user}`).length === 0) {
-        // $("#users-with-status-list").append(`<li id="submitted-answer-${event.user}">${event.user}</li>`)
-      }
     })
 
     RoomMessageHub.register(EventType.StartVoting, (event) => {
@@ -22,11 +18,8 @@ export default class extends Controller {
     })
 
     RoomMessageHub.register(EventType.VoteSubmitted, (event) => {
-      // user has voted; is on their /results page
+      // user has voted and is on their /results page
       console.log("RoomMessageHub:PromptController:Listener:VoteSubmitted", event)
-      if ($(`#vote-${event.user}`).length === 0) {
-        // $("#votes").append(`<li id="vote-${event.user}">${event.user}</li>`)
-      }
     })
 
     RoomMessageHub.register(EventType.VotingDone, (event) => {
