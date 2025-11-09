1bda943b4bbf4a1e4f1727f7b252a6ed2a72da6c
zachjustice <me@zachjustice.dev>
2025-11-09 14:15:46 -0500

    redirect prompts to current game phase

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -1,9 +1,11 @@
 class PromptsController < ApplicationController
+  before_action :redirect_to_current_game_phase
+
   def show
     exists = Answer.exists?(
       game_prompt_id: params[:id],
       user_id: @current_user.id,
-      game_id: @current_room.current_game_id
+      game: @current_room.current_game
     )
 
     # User already submitted an answer for this prompt; redirect to next page
@@ -41,9 +43,38 @@ class PromptsController < ApplicationController
 
   def results
     @status = @current_room.status
-    current_game_prompt_id = @current_room&.current_game&.current_game_prompt&.id
-    if @status == RoomStatus::Answering
-      redirect_to controller: "prompts", action: "show", id: current_game_prompt_id
+  end
+
+  private
+
+  def redirect_to_current_game_phase
+    controller = params[:controller]
+    action = params[:action]
+    id = params[:id].to_i
+    current_game_prompt_id = @current_room.current_game&.current_game_prompt&.id
+    case @current_room.status
+    when RoomStatus::WaitingRoom
+      if controller != "rooms" || id != @current_room.id || [ "show", "waiting_for_new_game" ].include?(action)
+        redirect_to controller: "rooms", action: "waiting_for_new_game", id: @current_room.id
+      end
+    when RoomStatus::Answering
+      if controller != "prompts" || id != current_game_prompt_id || ![ "show", "waiting" ].include?(action)
+        redirect_to controller: "prompts", action: "show", id: @current_room.current_game.current_game_prompt.id
+      end
+    when RoomStatus::Voting
+      if controller != "prompts" || id != current_game_prompt_id || ![ "voting", "results" ].include?(action)
+        redirect_to controller: "prompts", action: "voting", id: @current_room.current_game.current_game_prompt.id
+      end
+    when RoomStatus::Results
+      if controller != "prompts" || id != current_game_prompt_id || action != "results"
+        redirect_to controller: "prompts", action: "results", id: @current_room.current_game.current_game_prompt.id
+      end
+    when RoomStatus::FinalResults
+      if controller != "prompts" || id != current_game_prompt_id || action != "results"
+        redirect_to controller: "prompts", action: "results", id: @current_room.current_game.current_game_prompt.id
+      end
+    else
+      redirect_to controller: "rooms", action: "show"
     end
   end
 end
