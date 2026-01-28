5f753fbee0834c8db237dbb58410ef22cb98f08c
zachjustice <me@zachjustice.dev>
2026-01-28 17:57:38 -0500

    Run background job in e2e system test

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,6 +2,8 @@ require "test_helper"
 require "capybara/cuprite"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  include ActiveJob::TestHelper
+
   driven_by :cuprite, screen_size: [ 1400, 1400 ], options: {
     headless: true
   }
